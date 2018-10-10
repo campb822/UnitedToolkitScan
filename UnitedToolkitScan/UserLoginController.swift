e79c74acd4571ce2e97374f8a7bc3d3bb2ee78eb
Scott Campbell <campb822@msu.edu>
2018-10-10 11:41:19 -0400

    Login

--- UnitedToolkitScan/UserLoginController.swift
+++ UnitedToolkitScan/UserLoginController.swift
@@ -9,26 +9,69 @@
 import Foundation
 import UIKit
 
-class UserLogin: UIViewController{
+class UserLogin: UIViewController, UITextFieldDelegate{
     
-    @IBAction func username(_ sender: UITextField) {
-    }
-    @IBAction func password(_ sender: UITextField, forEvent event: UIEvent) {
-    }
-    @IBAction func loginButton(_ sender: UIButton) {
-    }
-    
-//    func nextButton(_ textField: UITextField) -> Bool {
-//        let nextTag = textField.tag + 1
-//        let nextResp = textField.superview?.viewWithTag(nextTag) as UIResponder!
-//
-//        if nextResp != nil {
-//            nextResp?.becomeFirstResponder()
-//        }
-//        else{
-//            textField.resignFirstResponder()
-//        }
-//        return false
-//    }
 
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    
+    
+    
+    @IBAction func login(_ sender: UIButton) {
+        loginButtonPressed()
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if (textField == username){
+            username.resignFirstResponder()
+            password.becomeFirstResponder()
+        }
+        else{
+            password.resignFirstResponder()
+            loginButtonPressed()
+        }
+        
+        return true
+    }
+    
+    func loginButtonPressed(){
+        if(username.text == "" || password.text == ""){
+            let alert = UIAlertController(title: "Missing Login Information", message: "A username or password was not entered. Please try again.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: nil))
+            
+            
+            self.present(alert, animated: true)
+        }
+        
+        else{
+            let params = ["username":username.text, "password":password.text] as! Dictionary<String, String>
+            
+            var request = URLRequest(url: URL(string: "http://35.9.22.103/admin")!)
+            request.httpMethod = "POST"
+            request.httpBody = try? JSONSerialization.data(withJSONObject: params, options: [])
+            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            
+            let session = URLSession.shared
+            let task = session.dataTask(with: request, completionHandler: { data, response, error -> Void in
+                print(response!)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data!) as! Dictionary<String, AnyObject>
+                    print(json)
+                } catch {
+                    print("error with database communication")
+                }
+            })
+            
+            task.resume()
+        }
+    }
 }
